90213b343012785d378c09aa93d80ddb3c00e4bf
YeshuaChiliquingaAmaya <yeshuachiliquinga@gmail.com>
2023-06-01 04:13:18 -0500

    OdontoApp V0.5.0

---
+++ 06-Code/OdontoAppV0.5.0/OdontoApp/src/ec/edu/espe/odontoapp/controller/pruebas.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class pruebas {
+
+    public static void main(String[] args) {
+        String csvFile = "Patients.csv";
+
+        // Leer el archivo CSV y cargar los datos en una lista
+        List<String[]> rows = readCSVFile(csvFile);
+
+        // Actualizar las filas con la enumeración
+        updateEnumeratedRows(rows);
+
+        // Guardar los datos actualizados en el archivo CSV
+        saveCSVFile(rows, csvFile);
+
+        System.out.println("Archivo CSV actualizado correctamente con la enumeración.");
+    }
+
+    private static List<String[]> readCSVFile(String fileName) {
+        List<String[]> rows = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(",");
+                rows.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rows;
+    }
+
+    private static void saveCSVFile(List<String[]> rows, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String[] row : rows) {
+                String line = String.join(",", row);
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateEnumeratedRows(List<String[]> rows) {
+        int enumeration = 1;
+
+        for (int i = 1; i < rows.size(); i++) {
+            String[] row = rows.get(i);
+            row[0] = String.valueOf(enumeration++);
+        }
+    }
+}
+
